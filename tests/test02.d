c1653edf7b2c9de32614975b64e036c322856fe3
alexrp <xtzgzorex@gmail.com>
2012-03-17 12:06:57 +0100

    More tests.

---
+++ tests/test02.d
@@ -0,0 +1,12 @@
+import gc;
+
+void main()
+{
+    GC_parallel = 1;
+
+    for (auto i = 0; i < 1024; i++)
+    {
+        auto mem = GC_malloc(size_t.sizeof * 4);
+        GC_gcollect();
+    }
+}
---
+++ tests/test03.d
@@ -0,0 +1,12 @@
+import gc;
+
+void main()
+{
+    GC_enable_incremental();
+
+    for (auto i = 0; i < 1024; i++)
+    {
+        auto mem = GC_malloc(size_t.sizeof * 4);
+        GC_gcollect();
+    }
+}
